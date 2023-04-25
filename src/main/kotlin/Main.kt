4f4fd23ace13494501188697b128ba9e9f29ee19
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-25 09:10:46 +0300

    kotlin assesment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+fun main (){
+    random()
+    password()
+
+
+}
+
+//Write and invoke one function that takes in a random string and returns the
+//following values: first character, last character, length of the string and
+//whether it begins with a vowel or not. (6pts)
+fun random(){
+    var random=("yvonne")
+    for(x in word){
+        if(word==word){
+            var len=word.length-[1]
+            println(word.length)
+        }
+        else if(word<)
+
+    }
+
+
+
+    //Create a function that takes in a password as a parameter. For a password to
+//be valid it must meet the following conditions:
+//1. Must be at least 8 characters long
+//2. Must be at most 16 characters long
+//3. Must not be “password”
+//4. Must contain a digit
+//Your function should determine whether the password provided is valid or not.
+//The function returns true/false (7pts)
+    fun password(){
+        var password=password():String
+
+    }
+
+
+
+
+    //create a function that print out multiples of 6 and  between 1 and 100.
+//for multiples of both 6 and  it should print out bingo
+    fun multiple(){
+        var multiple=[]
+
+    }
+
+    //create a class current account with the following attributes account number.
+//deposite(amount:double
+    class CurrentAccount=(accountNumber:Int,accountNumber:4444,"yvonne",6666 )
+    deposite(6547)
+    currentAccount.details()
+    withdraw.(3627)
+
+//create another class saving account itr has the same function.
+//attributes as current account
+
+
+
+
+
